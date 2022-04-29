0e4120ea620e18475332f7d0bc600fc7092c773c
MiriamCol <91241154+MiriamCol@users.noreply.github.com>
2022-04-29 17:16:01 +0200

    Añadido_LLamadaaApiJS_29deAbril

---
+++ src/main/resources/static/js/llamadaApi.js
@@ -0,0 +1,31 @@
+
+    function llamadaApi()
+    {
+
+        fetch('https://type.fit/api/quotes')
+            .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+           api(data);
+
+        })
+        .catch(function (err) {
+            console.log('error: ' + err);
+        });
+
+    }
+
+    function api(data)
+    {
+
+        let numero= Math.floor(Math.random()* (1642 - 0) + 0); //elegimos una frase aleatoria de entre toda la lista
+        alert(numero)
+        let frase=data[numero]["text"];
+        let autor=data[numero]["author"];
+        console.log(frase,autor);
+        document.getElementById("frase").innerHTML = frase;
+
+
+   	}
+
