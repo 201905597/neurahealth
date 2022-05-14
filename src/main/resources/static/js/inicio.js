fffe423978120ba5e23b77ea3bc7cfddf28fbfe9
201905597 <78108534+201905597@users.noreply.github.com>
2022-05-14 16:48:15 +0200

    Control de errores - inicio user y psic

--- src/main/resources/static/js/inicio.js
+++ src/main/resources/static/js/inicio.js
@@ -1,46 +1,96 @@
 // VERIFICAR CREDENCIALES DE USUARIO
-async function verificarUser(username,password,error){
+async function verificarUser(username,password,validu){
     event.preventDefault();
     let valid = false;
+    console.log(username);
+    console.log(password);
 
-    let res = await fetch("/api/v1/usuarios",{
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-    }});
-
-    if (res.status == 200){
-        const data = await res.json();
-        console.log(data);
-        let userId = "";
-        for (let i = 0; i<data.length; i++){
-            let user = data[i];
-            let userName = user["userName"];
-            let pass = user["password"];
-            userId = user["id"];
-            //SET del parámetro user ID
-            sessionStorage.setItem("userId",userId);
-
-            if (userName == username || pass == password){
-                valid = true;
-                break;
-            }
-        }
-        if (valid){
-            //sessionStorage.setItem("userId",userId);
-            location.replace("userIndex.html");
-            //console.log("hola");
-            //let userId2 = document.getElementById("userId");
-            //userId2.innerHTML = userId;
-        }else{
-            error.innerHTML = '<p style="color:red;">Hay algún error en los datos introducidos.</p>';
-        }
+    if (username == "" || password == ""){
+        alert("Por favor, rellena todos los campos");
     }else{
-        alert("¡Vaya! No se ha podido resolver tu petición");
+        let res = await fetch("/api/v1/usuarios",{
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+        }});
+
+        if (res.status == 200){
+            const data = await res.json();
+            console.log(data);
+            let userId = "";
+            for (let i = 0; i<data.length; i++){
+                let user = data[i];
+                let userName = user["userName"];
+                let pass = user["userPwd"];
+                userId = user["id"];
+                console.log(userName);
+                console.log(pass);
+                //SET del parámetro user ID
+                sessionStorage.setItem("userId",userId);
+                console.log(valid);
+                if (userName == username && pass == password){
+                    valid = true;
+                    break;
+                }
+            }
+            if (valid){
+                location.replace("userIndex.html");
+            }else{
+                validu.innerHTML = '<p style="color:red;">Hay algún error en los datos introducidos.</p>';
+            }
+        }else{
+            alert("¡Vaya! No se ha podido resolver tu petición");
+        }
     }
+
+
 }
 
 // VERIFICAR CREDENCIALES DE PSICÓLOGO
-async function verificarPsic(psicname,password){
+async function verificarPsic(psicname,pass,error){
+    event.preventDefault();
+    let valid = false;
+    console.log(psicname);
+    console.log(pass);
+
+    if (psicname == "" || pass == ""){
+        alert("Por favor, rellena todos los campos");
+    }else{
+        let res = await fetch("/api/v1/psicologos",{
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+        }});
+
+        if (res.status == 200){
+            const data = await res.json();
+            console.log(data);
+            let psicId = "";
+            for (let i = 0; i<data.length; i++){
+                let psic = data[i];
+                let psicData = psic["psicData"];
+                let psicName = psic["psicName"];
+                let password = psic["psicPwd"];
+                psicId = psic["id"];
+                console.log(psicName);
+                console.log(password);
+                //SET del parámetro user ID
+                sessionStorage.setItem("psicId",psicId);
+                console.log(valid);
+                if (psicName == psicname && password == pass){
+                    valid = true;
+                    break;
+                }
+            }
+            if (valid){
+                location.replace("psicIndex.html");
+            }else{
+                error.innerHTML = '<p style="color:red;">Hay algún error en los datos introducidos.</p>';
+            }
+        }else{
+            alert("¡Vaya! No se ha podido resolver tu petición");
+        }
+    }
 }
