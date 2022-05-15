497e4ce3e0ea59dfceda5a48c1f0eea646471194
201905597 <78108534+201905597@users.noreply.github.com>
2022-05-15 16:54:30 +0200

    UserController E2E Test - get all users test OK

---
+++ src/test/java/com/neura/neurahealth/controller/UsuarioControllerE2ETest.java
@@ -0,0 +1,59 @@
+package com.neura.neurahealth.controller;
+
+import com.neura.neurahealth.repository.UsuarioRepository;
+import com.neura.neurahealth.service.dto.UsuarioDTO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
+
+import java.util.List;
+import java.util.stream.StreamSupport;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class UsuarioControllerE2ETest {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @LocalServerPort
+    private int port;
+
+    @Test
+    public void getAllUsersE2ETest(){
+
+        //Given
+        List<UsuarioDTO> usuarios = StreamSupport.stream(usuarioRepository.findAll().spliterator(),false)
+                .map(obj -> new UsuarioDTO(
+                        obj.getId(),
+                        obj.getUserData(),
+                        obj.getUserName(),
+                        obj.getUserPwd(),
+                        obj.getIdPsic()
+                )).toList();
+
+        //When
+        String url = "http://localhost:" + Integer.toString(port) + "/api/v1/usuarios";
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<List<UsuarioDTO>> result = testRestTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                entity,
+                new ParameterizedTypeReference<List<UsuarioDTO>>() {}
+        );
+
+        //Then
+        then(result.getStatusCode()).isEqualTo(HttpStatus.OK);
+        then(result.getBody()).isEqualTo(usuarios);
+    }
+}
