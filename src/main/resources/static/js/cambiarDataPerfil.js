5a1d39d3f15e3bfffc6a587b962b24586e1a37ee
201905597 <78108534+201905597@users.noreply.github.com>
2022-05-15 13:21:05 +0200

    Update arreglado - faltan un par de cosas, pero ya no da error

--- src/main/resources/static/js/cambiarDataPerfil.js
+++ src/main/resources/static/js/cambiarDataPerfil.js
@@ -22,7 +22,8 @@ function showInfo()
         cambiado="cambiado_PSI";
     }
 
-
+    console.log("DIV OBJETIVO:");
+    console.log(divObjetivo.id);
 
     if (divObjetivo.style.display === "none"){
         for (let i=0;i<divs.length-1;i++){
@@ -30,7 +31,13 @@ function showInfo()
             divAEsconder.style.display = "none"; //Se esconde
         }
         divObjetivo.style.display = "block"; //Se muestra
-        divs[3].style.display = "block"; //Se muestra el boton de enviar siempre
+        console.log("print del array de divs y del div de mostrar2")
+
+        //divs[3].style.display = "block"; //Se muestra el boton de enviar siempre //esto daba error pk divs[3] es un string
+        //CAMBIOS DE CLAU
+        //let divMostrarEnviar = document.getElementById(divs[3]);
+        //divMostrarEnviar.style.display = "block"; //Se muestra el boton de enviar siempre -> esto estaría bien pero ese div no existe asi que no hace nada esta linea
+
     } else {
         divObjetivo.style.display = "none"; //Se esconde si estaba mostrado
     }
@@ -41,10 +48,12 @@ function showInfo()
 
 async function updateUsuario(){
 
-
+    event.preventDefault();
+    console.log("dentro de updateUsuario");
 
     var userId = sessionStorage.getItem('userId');
     sessionStorage.setItem("userId",userId);
+    console.log(userId);
 
     let api2 = "/api/v1/usuarios/" + userId.toString();
 
@@ -55,59 +64,71 @@ async function updateUsuario(){
             'Accept': 'application/json',
          }});
         if (res2.status == 200){
+            console.log("status del get es 200");
+            const data2 = await res2.json(); //Es un único usuario (no hay que recorrerlo con un for)
+            //console.log(data2);
+            let userData = data2["userData"];
+            let userNameNuevo = data2["userName"];
+            let passwNuevo = data2["userPwd"];
+            let psicNuevo = data2["idPsic"];
 
-               const data2 = await res2.json();
-                for (let j = 0; j<data2.length; j++){
-                     let usuario = data2[j];
-                     let userData = usuario["userData"];
-                     let userName = usuario["userName"];
-                     let userPwd = usuario["userPwd"];
-                     let psicNuevo = usuario["idPsic"];
-                     console.log(usuario);
-             }
+            //Si el fetch ha ido bien, seguimos con el update
+            console.log(cambiado);
+
+            if(cambiado=="cambiado_NU")
+            {
+                userNameNuevo = document.getElementById("userNameChange").value;
+                //userNameNuevo = userName.toString(); //esto haría que no se cambie el nombre de usuario!
+                console.log("Nuevo username: " + userNameNuevo);
+            }
+
+             else if(cambiado=="cambiado_PSW")
+             {
+                passwNuevo = document.getElementById("userPasswChange").value;
+                passwNuevo = passwNuevo.toString();
+
+            }
+            else if(cambiado=="cambiado_PSI")
+            {
+                psicNuevo = document.getElementById("psicIdChange").value;
+                psicNuevo = psicNuevo.toString();
+            }
+
+
+
+                const dataObj = {
+                            "id" : userId,
+                            "userData": userData,
+                            "userName" : userNameNuevo,
+                            "userPwd" : passwNuevo,
+                            "idPsic" : psicNuevo
+                        };
+
+                    //AQUI FALTARIA UN CONTROL DE ERRORES: ver qué status tiene res para saber si ha ido bien o mal
+                    //Si ha ido bien podría salir una alert de "Cambio realizado correctamente" y si no de "No se ha podido resolver la petición"
+                    //También faltarían dos comprobaciones:
+                    //(1) Comprobar que el nuevo nombre de usuario no está cogido (esto está hecho en registro.js por si te quieres basar en eso)
+                    //(2) Comprobar que la nueva contraseña es de mínimo 8 caracteres
+                     let api = "/api/v1/usuarios/" + userId.toString();
+
+                        let res = await fetch(api,{
+                        method: 'PUT',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify(dataObj)
+                    });
+
+                    console.log(res);
+                    //showUsers(); //esta funcion no esta definida en este javascript asi que da error
+                    //te puedes definir una que haga un GET de todos los usuarios al final y los imprima en console.log para ver si se ha cambiado bien
+                    //pero esto es solo para verlo nosotras, lo importante es comprobar el status de res
+
         } //por si el usuario no cambia nada, se guarda como ya estaba
+        else{
+            alert("¡Vaya! No se ha podido resolver tu petición.")
+        }
 
 
 
-    if(cambiado=="cambiado_NU")
-    {
-        userNameNuevo = document.getElementById("userNameChange").value;
-        userNameNuevo = userName.toString();
-    }
-
-     else if(cambiado="cambiado_PSW")
-     {
-        passwNuevo = document.getElementById("userPasswChange").value;
-        passwNuevo = passwNuevo.toString();
-
-    }
-    else if(cambiado="cambiado_PSI")
-    {
-        psicNuevo = document.getElementById("psicIdChange").value;
-        psicNuevo = psicNuevo.toString();
-    }
-
-
-
-        const dataObj = {
-                    "id" : userId,
-                    "userData": userData,
-                    "userName" : userNameNuevo,
-                    "userPwd" : passwNuevo,
-                    "idPsic" : psicNuevo
-                };
-
-             let api = "/api/v1/usuarios/" + userId.toString();
-
-                let res = await fetch(api,{
-                method: 'PUT',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(dataObj)
-            });
-
-            console.log(res);
-            showUsers();
-
-    }
+}
